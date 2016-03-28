4e542c7429507cac267ba5a7dd38e7bb4b8db723
TFR <TFR@tfrs-macbook-pro.local>
2016-03-28 19:43:25 +0200

    set maxRows for each comboBox

--- FYComboBoxExample/FYComboBoxExample/ViewController.m
+++ FYComboBoxExample/FYComboBoxExample/ViewController.m
@@ -16,6 +16,9 @@
     self.languages = @[@"Objective-C", @"Swift", @"Python", @"C++", @"C"];
     self.colors = @[@"Red", @"Orange", @"Green", @"Blue", @"Yellow", @"Black", @"White", @"Gray"];
     
+    self.languagesComboBox.layer.borderWidth = 1.f;
+    self.languagesComboBox.layer.cornerRadius = 15.f;
+
     self.colorsComboBox.layer.borderWidth = 1.f;
     self.colorsComboBox.layer.borderColor = [UIColor blackColor].CGColor;
     self.colorsComboBox.layer.cornerRadius = 15.f;
@@ -27,9 +30,10 @@
 - (NSInteger)comboBoxNumberOfRows:(FYComboBox *)comboBox
 {
     if (comboBox == self.languagesComboBox) {
+        self.languagesComboBox.maxRows = self.languages.count;
         return self.languages.count;
-    
     } else if (comboBox == self.colorsComboBox) {
+        self.colorsComboBox.maxRows = self.colors.count;
         return self.colors.count;
     }
     
@@ -40,7 +44,6 @@
 {
     if (comboBox == self.languagesComboBox) {
         return self.languages[row];
-        
     } else if (comboBox == self.colorsComboBox) {
         return self.colors[row];
     }
@@ -53,7 +56,6 @@
     if (comboBox == self.languagesComboBox) {
         NSLog(@"Selected %@", self.languages[row]);
         self.languageLabel.text = self.languages[row];
-    
     } else if (comboBox == self.colorsComboBox) {
         NSLog(@"Selected %@", self.colors[row]);
         self.colorLabel.text = self.colors[row];
